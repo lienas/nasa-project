fa484a2ec2a54c6bc3e552c7233b50fe8a4bb46a
Thomas Lucas <t.lucas@lienas.de>
2022-02-10 17:12:05 +0100

    make some progress till 114

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,4 +1,4 @@
-const planets = require('../../models/planates.model');
+const {planets} = require('../../models/planets.model');
 
 function getAllPlanets(req, res) {
     return res.status(200).json(planets);
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,23 @@
 const http = require('http');
 const app = require('./app');
+const {loadPlanetsData} = require("./models/planets.model");
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000;
 
-server.listen(PORT, () => {
-    console.log(`Listen in Port ${PORT}`);
-});
+
+async function startServer() {
+
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+        console.log(`Listen in Port ${PORT}`);
+    });
+}
+
+startServer();
+
 
 
 
