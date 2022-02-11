71fbfbe3d8d41fe5215592da6251853fe5ee3f8f
thomas <t.lucas@lienas.de>
2022-02-11 07:47:22 +0100

    integrate client - 117

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const planetsRouter = require('./routes/planets/planets.router');
+const path = require("path");
 const cors = require('cors');
+const morgan = require("morgan");
+
+const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
 app.use(cors({
-    origin:'http://localhost:3000'
+    origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+console.log(path.join(__dirname, '..', 'public'));
+
 app.use(planetsRouter);
+app.get('/', ((req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+}))
 
 module.exports = app;
